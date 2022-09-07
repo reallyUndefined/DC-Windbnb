39fc8b62768adef1bcc238d77eb457fc29f46950
Prabu <prabu.undefined@gmail.com>
2022-09-07 18:07:36 +0530

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        mulish: ["Mulish", "sans-serif"],
+      },
+      colors: {
+        lightGray: "#828282",
+        darkGray: "#333333",
+        softRed: "#EB5757",
+        overlay: "rgba(79, 79, 79, 0.4)",
+      },
+    },
+  },
+  plugins: [],
+};

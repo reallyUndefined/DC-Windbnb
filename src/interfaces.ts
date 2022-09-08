2bb020c4445fd640d5e9383a77bd0708d35f4314
Prabu <prabu.undefined@gmail.com>
2022-09-08 09:07:33 +0530

    added interface for data and renamed header component file

---
+++ src/interfaces.ts
@@ -0,0 +1,11 @@
+export interface iStay {
+  city: string;
+  country: string;
+  superHost: boolean;
+  title: string;
+  rating: number;
+  maxGuests: number;
+  type: string;
+  beds: number;
+  photo: string;
+}

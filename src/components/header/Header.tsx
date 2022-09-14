da51d719028870e6baaf03831a7f80fa426a1aa1
Prabu <prabu.undefined@gmail.com>
2022-09-14 09:03:47 +0530

    displaying filters in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,16 @@
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import logo from "../../logo.png";
+import { FiltersState } from "../../redux/filters/filters";
 
 interface HeaderProps {
   setShowSearchMenu: React.Dispatch<React.SetStateAction<boolean>>;
 }
 function Header({ setShowSearchMenu }: HeaderProps) {
+  const filters = useSelector(
+    (state: { filters: { value: FiltersState } }) => state.filters.value
+  );
+
   return (
     <header className="pt-4 mb-9 md:flex items-center justify-between md:pt-8 md:mb-14">
       <img className="cursor-pointer" src={logo} alt="logo" />
@@ -12,11 +18,21 @@ function Header({ setShowSearchMenu }: HeaderProps) {
         className="rounded-2xl overflow-hidden mx-auto mt-9 custom-shadow flex items-center max-w-[18.75rem] w-full md:m-0"
         onClick={() => setShowSearchMenu(true)}
       >
-        <div className="w-full p-4 border-r-[1px] text-sm text-lightGray flex-2 cursor-pointer">
-          Add location
+        <div
+          className={`w-full p-4 border-r-[1px] text-sm ${
+            filters.location.length > 0 ? "text-mediumGray" : "text-lightGray"
+          } flex-2 cursor-pointer whitespace-nowrap overflow-hidden text-ellipsis`}
+        >
+          {filters.location.length > 0 ? filters.location : "Add location"}
         </div>
-        <div className="w-full p-4 border-r-[1px] text-sm text-lightGray flex-2 cursor-pointer">
-          Add guests
+        <div
+          className={`w-full p-4 border-r-[1px] text-sm ${
+            filters.totalGuests > 0 ? "text-mediumGray" : "text-lightGray"
+          } flex-2 cursor-pointer whitespace-nowrap overflow-hidden text-ellipsis`}
+        >
+          {filters.totalGuests > 0
+            ? `${filters.totalGuests} guests`
+            : "Add guests"}
         </div>
         <button aria-label="search" className="flex-1 h-full p-4">
           <FaSearch className="text-softRed" />

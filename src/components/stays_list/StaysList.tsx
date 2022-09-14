231d2ad04d3af45060bc32bc871adcfffd912fac
Prabu <prabu.undefined@gmail.com>
2022-09-14 08:53:23 +0530

    filtering logic in StaysList

--- src/components/stays_list/StaysList.tsx
+++ src/components/stays_list/StaysList.tsx
@@ -1,3 +1,5 @@
+import { useSelector } from "react-redux";
+import { FiltersState } from "../../redux/filters/filters";
 import { iStay } from "../../typescript_stuff/interfaces";
 import StayCard from "../stay_card/StayCard";
 
@@ -5,16 +7,39 @@ interface StaysListProps {
   staysData: iStay[];
 }
 function StaysList({ staysData }: StaysListProps) {
+  const filters = useSelector(
+    (state: { filters: { value: FiltersState } }) => state.filters.value
+  );
+
+  const filteredList = staysData.filter((data) => {
+    let res;
+
+    const [city, country] = filters.location.split(", ");
+    const locationMatch =
+      city === "" && country === undefined
+        ? true
+        : city === data.city && country === data.country;
+
+    if (city === "" && filters.totalGuests === 0) res = data;
+    else if (locationMatch && filters.totalGuests <= data.maxGuests) {
+      res = data;
+    }
+    return res;
+  });
+
+  const staysCount =
+    filteredList.length > 12 ? `12+ stays` : `${filteredList.length} stays`;
+
   return (
     <div className="font-medium mb-8 md:mb-16">
       <div className="flex justify-between items-center mb-5 md:mb-8">
         <h1 className="text-lg text-darkGray font-bold md:text-2xl">
           Stays in Finland
         </h1>
-        <span className="text-mediumGray text-sm">12+ stays</span>
+        <span className="text-mediumGray text-sm">{staysCount}</span>
       </div>
       <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {staysData.map((data, i) => (
+        {filteredList.map((data, i) => (
           <StayCard stay={data} key={i} />
         ))}
       </div>
